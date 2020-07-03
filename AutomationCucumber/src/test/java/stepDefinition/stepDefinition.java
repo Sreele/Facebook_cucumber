3e28fb2d9154b3a8a3763df49c5f0a22eb1f448a
Sreele <64038779+Sreele@users.noreply.github.com>
2020-07-03 12:43:38 +0530

    Update stepDefinition.java
    add comment

--- AutomationCucumber/src/test/java/stepDefinition/stepDefinition.java
+++ AutomationCucumber/src/test/java/stepDefinition/stepDefinition.java
@@ -17,10 +17,13 @@ public class stepDefinition {
 	WebDriver driver;
 	@Given("^launch browser and navigate to url$")
     public void launch_browser_and_navigate_to_url() throws Throwable {
-       
+                // Configuring the system properties of chrome driver
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\GAVSLTPBCP\\Downloads\\chromedriver_win32\\chromedriver.exe");
+	        //Initializing the browser driver
 		driver= new ChromeDriver();
+	        // maximize the browser window
 		driver.manage().window().maximize();
+	        //navigating to url
 		driver.get("https://www.facebook.com/");
 		
     }
@@ -28,22 +31,23 @@ public class stepDefinition {
 	@When("^user login into application with (.+) and password (.+)$")
     public void user_login_into_application_with_and_password(String username, String password) throws Throwable {
 		System.out.println(username);
-	    System.out.println(password);
-	    driver.findElement(By.xpath("//input[@id='email']")).sendKeys(username);
+	        System.out.println(password);
+	        //sending values into textboxes
+	        driver.findElement(By.xpath("//input[@id='email']")).sendKeys(username);
 		driver.findElement(By.xpath("//input[@id='pass']")).sendKeys(password);
 		driver.findElement(By.xpath("//input[@id='u_0_b']")).click();
     }
 
     @Then("^get the titile of page loaded$")
     public void get_the_titile_of_page_loaded() throws Throwable {
-    	//get the titile of page loaded
-    	String Title = driver.getTitle(); 	
+    	        //get the titile of page 
+    	        String Title = driver.getTitle(); 	
 		System.out.println(Title);
     }
     @Then("^close the browser$")
     public void close_the_browser() throws Throwable {
-    	//close the browser
-    	driver.close();
+		//close the browser
+		driver.close();
     }
 
 
